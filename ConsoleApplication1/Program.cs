8f87f9473db5acf6e3974ac3f212d91d49b984db
Waqas Javed Javed <Waqas Javed Javed>
2016-10-22 15:44:50 +0500

    Parsing, finding onechar difference, linking

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Runtime.Serialization;
 using Newtonsoft.Json.Linq;
+using System.Threading;
 
 namespace ConsoleApplication1
 {
@@ -14,26 +15,56 @@ namespace ConsoleApplication1
 
         public static bool onedifference(string s1, string s2)
         {
-            int count = 0;
-            for (int i = 0; i < s1.Length;i++ )
+            if (s1.Length == s2.Length)
             {
-                if (count <= 1)
+                int count = 0;
+                for (int i = 0; i < s1.Length; i++)
                 {
-                    if (s1[i] != s2[i])
+                    if (count <= 1)
                     {
-                        count++;
+                        if (s1[i] != s2[i])
+                        {
+                            count++;
+                        }
+                    }
+                    else
+                    {
+                        break;
                     }
                 }
-                else {
+
+                if (count == 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static void findlink(List<string> words, string x, ref List<List<string>> link) {
+            List<string> temp = new List<string>();
+
+            temp.Add(x);
+            foreach (var y in words)
+            {
+
+                if (x != y && x.Length == y.Length)
+                {
+                    if (onedifference(x, y))
+                    {
+                        temp.Add(y);
+                        //Console.Out.WriteLine(x + ":" + y + ":");
+                    }
+                }
+                else if (y.Length > x.Length)
+                {
                     break;
                 }
             }
 
-            if(count  == 1){
-                return true;
-            }
-            return false;
+            link.Add(temp);
         }
+
         static void Main(string[] args)
         {
             DateTime start_time;
@@ -60,28 +91,69 @@ namespace ConsoleApplication1
             Console.Out.WriteLine("Content Read and Parsed Successfully in Time: " + Convert.ToString( end_time-start_time));
             Console.Out.WriteLine("Total Words: " + words.Count);
 
-            //creating adjecncy matrix
+
+            Console.Out.WriteLine("Finding links Please wait");
 
             List<List<string>> link = new List<List<string>>();
+            List<string> temp = new List<string>();
+            start_time = DateTime.Now;
+            int cou = 0;
 
 
-
+            //traverse and find links
             foreach(var x in words){
-                foreach (var y in words)
-                {
-                    if(x != y && x.Length == y.Length){
-                        if (onedifference(x,y))
-                        {
-                            //Console.Out.WriteLine(x + ":" + y + ":");
-                        }
-                    }else if(y.Length > x.Length){
-                       // break;
-                    }
-                }
+                //findlink(words, x, ref link);
+                bool flag = false;
+                //temp = new List<string>();
+                //temp.Add(x);
+
+                var result = from w in words
+                                 where onedifference(w, x)
+                                 select w;
+                  
+                //foreach (var y in words)
+                //{
+                    
+                //    if (x != y && x.Length == y.Length)
+                //    {
+                //        if (onedifference(x, y))
+                //        {
+                //            temp.Add(y);
+                //            Console.Out.WriteLine(x + ":" + y + ":");
+                //        }
+                //    }
+                //    else if (y.Length > x.Length)
+                //    {
+                //        flag = true;
+                //        break;
+                //    }
+
+                //}
+
+
+                //foreach (var y in result)
+                //{
+
+                //    Console.Out.WriteLine(x+":"+y);
+                //}
+                
+                link.Add(result.ToList());
+                if(cou%1000 == 0){
+                    Console.WriteLine(cou++);
+                };
+                cou++;
+                //temp.Clear();  
             }
 
-            Console.Out.WriteLine("Done");
+            end_time = DateTime.Now;
+            Console.Out.WriteLine("Done in time: " + (end_time-start_time));
 
+            var count = (from w in link
+                         where w.Count >= 1
+                         select w).Count();
+
+            Console.Out.WriteLine("Total Nodes: " + count);
+ 
 
                 //foreach(var x in words){
                 //    Console.Out.WriteLine(x);
